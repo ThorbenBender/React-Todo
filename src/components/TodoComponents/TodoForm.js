bcad130f6a7100e23ab96137da4b11b36c3e85af
Thorben Bender <bender.thorben0@gmail.com>
2019-02-13 13:37:11 +0100

    working on todolist.js

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Todo.css';
+
+export default class TodoForm {
+    render() {
+        return (
+            <div>
+                <input></input>
+                <button onClick={}></button>
+                <button onClick={}></button>
+            </div>
+        );
+    }
+}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import './Todo.css';
+
+export default function TodoList() {
+    return (
+        props.friends.map((friend, idx) => (
+            <div key={idx}>{todoName}</div>
+        ))
+    );
+}
